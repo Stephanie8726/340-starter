85329103ad23a9c7b01d5df28c3068e5d9ad0cbf
Stephanie Dacullo <stephanie.laruga101@yahoo.com>
2024-11-04 20:53:46 -0800

    week 1 - learning activity

--- routes/static.js
+++ routes/static.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express'); // assigned to a local variable
+const router = express.Router(); // stored into a local variable for use => Router is a function
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
+router.use(express.static("public")); // where static resources will be found
 router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
 
+// If a resource is NOT exported, it cannot be used somewhere else.
 module.exports = router;
 
 
--- server.js
+++ server.js
@@ -6,14 +6,36 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * View Engine and Templates
+ *************************/
+
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
+
+/* ***********************
+*** Index router
+* app.get => the expresses application will watch the "get" object, within the HTTP Request, for a particular route.
+* "/" => This is route being watched.
+* function(req, res){ => A JavaScript function that takes the request and response objects as parameters.
+* res.render() => The "res" is the response object, while "render()" is an Express function that will retrieve the specified view - "index" - to be sent back to the browser.
+* {title: "Home" } => The curly braces are an object (treated like a variable), which holds a name - value pair. This object supplies the value that the "head" partial file expects to receive. The object is passed to the view.
+* }) => The right curly brace closes the function, while the right parentheses closes the "get" route.
+ *************************/
+app.get("/", function(req, res){
+  res.render("index", {title: "CSE 340 App"})
+})
 
 /* ***********************
  * Local Server Information
